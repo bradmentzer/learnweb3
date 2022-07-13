2e9b1c5aa31fa729ed10d32499a65d1eca0ea257
Bradley Mentzer <bradleymentzer@gmail.com>
2022-07-13 09:25:43 -0400

    Add test deployment for Polygon Mainnet

--- senior/flash-loans/config.js
+++ senior/flash-loans/config.js
@@ -1,3 +1,5 @@
+// Hardhat feature called impersonation allows transactions on behalf of any address in local network
+
 // Mainnet DAI Address
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 // Random user's address that happens to have a lot of DAI on Polygon Mainnet
